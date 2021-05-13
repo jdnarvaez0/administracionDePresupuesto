3c9980b62a669f4866090dedaefc8daf8047ac66
navidev0 <juand.ns07@gmail.com>
2021-05-13 17:41:38 -0300

    Mostrar los gastos en el HTML

--- js/app.js
+++ js/app.js
@@ -17,37 +17,75 @@ class Presupuesto{
         this.restante = Number(presupuesto);
         this.gastos = [];
     }
+    nuevoGasto(gasto){
+        this.gastos = [...this.gastos, gasto];
+        console.log(this.gastos);
+    }
 }
 
-class IU{
-    insertarPresupuesto(cantidad){
-        const {presupuesto, restante} = cantidad;
+class IU {
+  insertarPresupuesto(cantidad) {
+    const { presupuesto, restante } = cantidad;
 
-        //agregar al HMTL
-        document.querySelector('#total').textContent = presupuesto;
-        document.querySelector('#restante').textContent = restante;
+    //agregar al HMTL
+    document.querySelector("#total").textContent = presupuesto;
+    document.querySelector("#restante").textContent = restante;
+  }
+
+  imprimirAlertas(mensaje, tipo) {
+    //crear div
+    const divMensaje = document.createElement("div");
+    divMensaje.classList.add("text-center", "alert");
+
+    if (tipo === "error") {
+      divMensaje.classList.add("alert-danger");
+    } else {
+      divMensaje.classList.add("alert-success");
     }
 
-    imprimirAlertas(mensaje, tipo){
-        //crear div
-        const divMensaje = document.createElement('div');
-        divMensaje.classList.add('text-center', 'alert');
-        
-        if (tipo === 'error') {
-            divMensaje.classList.add('alert-danger');
-        }else{
-            divMensaje.classList.add('alert-success');
-        }
+    //mensaje de error
+    divMensaje.textContent = mensaje;
+    document.querySelector(".primario").insertBefore(divMensaje, formulario);
 
-        //mensaje de error
-        divMensaje.textContent = mensaje;
-        document.querySelector('.primario').insertBefore(divMensaje, formulario);
+    //quitar del HMTL
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
 
-        //quitar del HMTL
-        setTimeout(() => {
-            divMensaje.remove();
-        }, 3000);
-    }
+  agregarGastosListado(gastos) {
+
+    this.limpiarHTML(); //elimina el html previo
+
+    //iterar sobre los gastos
+    gastos.forEach((gasto) => {
+      const { cantidad, nombre, id } = gasto;
+
+      // crer LI
+      const nuevoGasto = document.createElement("li");
+      nuevoGasto.className =
+        "list-group-item d-flex justify-content-between aling-items-center";
+      nuevoGasto.dataset.id = id;
+
+      //agergar html del gasto
+      nuevoGasto.innerHTML = `${nombre}<span class=" badge badge-pill badge-primary ">${cantidad}</span>`;
+
+      //Boton para borrar gasto
+      const btnBorrar = document.createElement("button");
+      btnBorrar.classList.add("btn", "btn-danger", "borrar-gasto");
+      btnBorrar.innerHTML = "Borrar &times";
+      nuevoGasto.appendChild(btnBorrar);
+
+      //agergar al html
+      gastoListado.appendChild(nuevoGasto);
+    });
+  }
+
+  limpiarHTML(){
+      while (gastoListado.firstChild ) {
+         gastoListado.removeChild(gastoListado.firstChild) ;
+      }
+  }
 }
 // instanciar
 const iu = new IU();
@@ -74,16 +112,28 @@ function agregarGastos(e) {
     
     //leer datos del formulario
     const nombre = document.querySelector('#gasto').value; 
-    const cantidad = document.querySelector('#cantidad').value;
+    const cantidad = Number(document.querySelector('#cantidad').value);
 
     //validar
     if (nombre === '' || cantidad === '') {
         iu.imprimirAlertas('Ambos campos son obligatorios' , 'error');
 
         return;
-    }else if(cantidad <= 0 || isNaN(cantidadi)){
+    }else if(cantidad <= 0 || isNaN(cantidad)){
         iu.imprimirAlertas('Cantidad no valida' , 'error')
 
         return;
     }
+    // generar un objeto con el gasto
+    const gasto = { nombre, cantidad, id:Date.now()};
+    presupuesto.nuevoGasto(gasto);
+
+    iu.imprimirAlertas('Gasto agregado Correctamente');
+
+    //Imprimir los gastos
+    const { gastos } = presupuesto;
+    iu.agregarGastosListado(gastos);
+    
+    //reinicio el formulario
+    formulario.reset();
 }
